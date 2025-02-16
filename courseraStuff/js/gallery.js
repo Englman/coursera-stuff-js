63bc5f8bdfec23b2d972cd81fb4e2a46e00f8c73
Englman <17042005v@gmail.com>
2025-02-16 14:49:48 +0500

    Add files via upload

---
+++ courseraStuff/js/gallery.js
@@ -0,0 +1,19 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    console.log("Hovered on " + previewPic.src);
+  
+    console.log("Source of the Image:" + previewPic.src);
+    console.log("Alt text:" + previewPic.alt);
+  
+    let imageContainer = document.getElementById("image");
+    imageContainer.style.backgroundImage = `url("${previewPic.src}")`;
+    imageContainer.innerHTML = previewPic.alt;
+	}
+
+	function unDo(){
+     let imageContainer = document.getElementById("image");
+    
+      imageContainer.style.backgroundImage = "url()";
+      imageContainer.innerHTML = "Okay";
+	}
